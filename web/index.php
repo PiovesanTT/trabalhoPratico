83b28befc4c5bd91d90b74218291fb9828c6b6f5
Jean Carlo <43098564+jeancarlo2@users.noreply.github.com>
2018-11-06 14:46:55 -0300

    index modificado para chamar as paginas certas

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>AdministraÃ§Ã£o - LocaPlus</title>
+        <title>Administração - LocaPlus</title>
 
         <!-- Bootstrap core CSS-->
         <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -32,23 +32,13 @@ and open the template in the editor.
         ?>
         <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-      <a class="navbar-brand mr-1" href="index.html">AdministraÃ§Ã£o</a>
+      <a class="navbar-brand mr-1" href="index.html">Administração</a>
 
       <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
       </button>
 
-      <!-- Navbar Search -->
-      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-          <div class="input-group-append">
-            <button class="btn btn-primary" type="button">
-              <i class="fas fa-search"></i>
-            </button>
-          </div>
-        </div>
-      </form>
+      
 
       <!-- Navbar -->
       <ul class="navbar-nav ml-auto ml-md-0">
@@ -96,7 +86,7 @@ and open the template in the editor.
       <!-- Sidebar -->
       <ul class="sidebar navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="index.html">
+          <a class="nav-link" href="index.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span>
           </a>
